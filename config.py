f6ebdb612c00ce22eb57a25b6de981204c25c3f9
Floris <floris.vanhengel@telenet.be>
2022-04-30 15:19:10 +0200

    Commit #6

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 
-PORT = 8888
+PORT = 80
 
 CERT_PATH = "./SSL/ssl.cert"
 KEY_PATH = "./SSL/ssl.key"
--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import config
 # Run the webserver
 Webserver = webserver.Webserver(port=config.PORT, devmode=True, daemon=True)
 
-Webserver.addSSL(certPath=config.CERT_PATH, keyPath=config.KEY_PATH,)
+def testfunc():
+    print("loopback")
+Webserver.addCallback(testfunc)
+#Webserver.addSSL(certPath=config.CERT_PATH, keyPath=config.KEY_PATH,)
 if __name__ == '__main__':
     Webserver.startWebServer()
+
+    
